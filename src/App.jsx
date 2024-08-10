c93e5cdc537c94a2c30ef522e3ae7ad85f76ca13
hrmrakib <hrmrakibs@gmail.com>
2024-08-10 12:09:34 +0600

    set working alert

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 export default function App() {
   setTimeout(() => {
     alert("Thank you for visting, I am working on the project right now!");
-  }, 10);
+  }, 5000);
   return (
     <div className='font-kanit'>
       <Home />
